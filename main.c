ed5e97b62e33755720d6e65fb0f9a71f6dc3dccc
meranhany25 <meranhany25@gmail.com>
2023-10-18 08:19:36 -0700

    main

--- main.c
+++ main.c
@@ -1,54 +1,50 @@
 #include "shell.h"
-char **commands = NULL;
-char *line = NULL;
-char *shell_name = NULL;
-int status = 0;
-
+	char **commands = NULL;
+	char *line = NULL;
+	char *shell_name = NULL;
+	int status = 0;
 /**
- * shell_main - The main function of the shell program.
+ * main - The main function of the shell program.
  * @argc: The number of arguments passed to the program.
  * @argv: The program arguments to be parsed.
  * Return: 0 on success.
  */
-int shell_main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	char **current_command = NULL;
-	int i, command_type = 0;
-	size_t buffer_size = 0;
+	int i, type_command = 0;
+	size_t n = 0;
 
-	signal(SIGINT, handle_ctrl_c);
+	signal(SIGINT, ctrl_c_handler);
 	shell_name = argv[0];
 	while (1)
 	{
-		process_non_interactive_mode();
-		custom_print(" ($) ", STDOUT_FILENO);
-		if (getline(&line, &buffer_size, stdin) == -1)
+		non_interactive();
+		print(" ($) ", STDOUT_FILENO);
+		if (getline(&line, &n, stdin) == -1)
 		{
 			free(line);
 			exit(status);
 		}
-
-		remove_newline(line);
-		remove_comment(line);
-		commands = split_string(line, ";");
+			remove_newline(line);
+			remove_comment(line);
+			commands = tokenizer(line, ";");
 
 		for (i = 0; commands[i] != NULL; i++)
 		{
-			current_command = split_string(commands[i], " ");
+			current_command = tokenizer(commands[i], " ");
 			if (current_command[0] == NULL)
 			{
 				free(current_command);
 				break;
 			}
+			type_command = parse_command(current_command[0]);
 
-			command_type = determine_command_type(current_command[0]);
-			execute_command_handler(current_command, command_type);
+			initializer(current_command, type_command);
 			free(current_command);
 		}
-
 		free(commands);
 	}
-
 	free(line);
 
 	return (status);
