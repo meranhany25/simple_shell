07ea9882d5058e6f239dbfc637c2b7bc9a146478
meranhany25 <meranhany25@gmail.com>
2023-10-18 08:04:18 -0700

    builtin_fuctions.c

--- builtin_fuctions.c
+++ builtin_fuctions.c
@@ -1,54 +1,49 @@
 #include "shell.h"
 /**
- * print_environment_variables - Prints the current environment variables.
+ * env - Prints the current environment variables.
  * @tokenized_command: The command entered.
- * Description: This function prints the current environment variables
- *              to the standard output.
+ * Description: This function displays environment variables on output.
  * Return: void.
  */
-void print_environment_variables(char **tokenized_command
-		__attribute__((unused)))
+void env(char **tokenized_command __attribute__((unused)))
 {
 	int i;
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		custom_print(environ[i], STDOUT_FILENO);
-		custom_print("\n", STDOUT_FILENO);
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
 	}
 }
 /**
- * exit_shell - Exits the shell.
+ * quit - Exits the shell.
  * @tokenized_command: The command entered.
- * Description: This function exits the shell. If no argument is provided,
- *              it exits with the current status. If an argument is provided,
- *              it exits with the specified status value.
+ * Description: No arg exits with
+ * current status arg exits with specified status.
  * Return: void.
  */
-void exit_shell(char **tokenized_command)
+void quit(char **tokenized_command)
 {
-	int num_tokens = 0, arg;
+	int num_token = 0, arg;
 
-	for (; tokenized_command[num_tokens] != NULL; num_tokens++)
+	for (; tokenized_command[num_token] != NULL; num_token++)
 		;
-
-	if (num_tokens == 1)
+	if (num_token == 1)
 	{
 		free(tokenized_command);
 		free(line);
 		free(commands);
 		exit(status);
 	}
-	else if (num_tokens == 2)
+	else if (num_token == 2)
 	{
-		arg = convert_string_to_integer(tokenized_command[1]);
-
+		arg = _atoi(tokenized_command[1]);
 		if (arg == -1)
 		{
-			custom_print(shell_name, STDERR_FILENO);
-			custom_print(": 1: exit: Illegal number: ", STDERR_FILENO);
-			custom_print(tokenized_command[1], STDERR_FILENO);
-			custom_print("\n", STDERR_FILENO);
+			print(shell_name, STDERR_FILENO);
+			print(": 1: exit: Illegal number: ", STDERR_FILENO);
+			print(tokenized_command[1], STDERR_FILENO);
+			print("\n", STDERR_FILENO);
 			status = 2;
 		}
 		else
@@ -60,7 +55,5 @@ void exit_shell(char **tokenized_command)
 		}
 	}
 	else
-	{
-		custom_print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
-	}
+		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 }
